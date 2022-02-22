12cb7605edbd714c1f3e57083acc3acf109959ab
NiceRob <96066353+NiceRob@users.noreply.github.com>
2022-02-22 10:21:50 +0100

    Add files via upload

---
+++ node_scanfolder.js
@@ -0,0 +1,30 @@
+// recursive scan folder for files and folder and write a files.json file as output
+const fs = require('fs');
+const path = require('path');
+
+const isFile = (fileName) => {
+  return fs.lstatSync(fileName).isFile();
+};
+
+const readFolder = (folderPath) => {
+  let categories = [];
+  const files = fs.readdirSync(folderPath).map((fileName) => {
+    return path.join(folderPath, fileName);
+  });
+  files.forEach((file) => {
+    let fileObj = {
+      name: file.replace(folderPath + '\\', ''),
+      id: file.replace(folderPath + '\\', '')
+    };
+    if (file !== 'node_modules' && isFile(file) === false) {
+      fileObj.children = readFolder(file);
+    }
+    categories.push(fileObj);
+  });
+  return categories;
+};
+// write the JSON file
+fs.writeFile('files.json', JSON.stringify(readFolder('./')), (err) => {
+  if (err) throw err;
+  console.log('Saved!');
+});
